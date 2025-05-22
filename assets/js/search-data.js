e95e16e51a554b4104f2ae4ce1d634391e9c3b14
AmirShahhosseini <137333334+AmirShahhosseini@users.noreply.github.com>
2025-05-22 16:50:26 +0000

    Deploying to gh-pages from @ AmirShahhosseini/amirshahhosseini.github.io@f373df2fe2a45bf1479d95ecfacf89945db80922 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A list of projects I&#39;ve done throughout my academic career!",
           section: "Navigation",
           handler: () => {
